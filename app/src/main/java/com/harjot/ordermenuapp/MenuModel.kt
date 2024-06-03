c9caee869b0f63be94b3a4b8e0d3a1c8ccec085d
harjots90414 <harjot90414singh@gmail.com>
2024-06-03 21:47:16 +0530

    changes

--- app/src/main/java/com/harjot/ordermenuapp/MenuModel.kt
+++ app/src/main/java/com/harjot/ordermenuapp/MenuModel.kt
@@ -1,6 +1,11 @@
 package com.harjot.ordermenuapp
 
-data class MenuModel(var name:String?=null, var price: Double? = 0.0, var quantity:Int=1){
+data class MenuModel(
+    var name:String?=null,
+    var price: Double? = 0.0,
+    var quantity:Int=1
+)
+{
     override fun toString(): String {
         return "$name"
     }
